aa2307dfda85ace77f04fe4397eddb5d3d6f8201
Qing Ji <qingji@stanford.edu>
2023-01-13 15:06:24 -0800

    changed prog.py

--- prog.py
+++ prog.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 
-
-
-def printMany(word:str,n:int)
+def printMany(word:str, n:int):
   """ I print word n times"""
-  for i in n:
+  for i in range(n):
     print(word)
 
 
-
-
-if __name __ == "__main__":
-  printMany("Hello world",10)
+if __name__ == "__main__":
+  printMany("Hello world", 10)
